2128452d2c548f6a75ad7cc0ac08d54fb209a1f6
coleyoung-game <ighong11@gmail.com>
2026-08-18 22:23:21 +0900

    [Update] 1강 강의 내용 코드 작성 완료

--- Assets/Internal/Scripts/Input/InputManager.cs
+++ Assets/Internal/Scripts/Input/InputManager.cs
@@ -1,20 +1,40 @@
 using System;
+using System.Diagnostics;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Debug = UnityEngine.Debug;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class InputManager : MonoBehaviour
 {
     private RaycastHit _hit;
     public Vector2 Position { get; private set; }
     public event Action OnAction_RightClick;
+    public event Action<RaycastHit> OnAction_RaycastHit;
     
     public void LeftClickAction(InputAction.CallbackContext ctx)
     {
         InputActionPhase phase = ctx.phase;
+
         switch (phase)
         {
             case InputActionPhase.Performed:
                 Debug.Log("Left Click Performed");
+                bool isUI = IsUITouch(Position);
+                if(isUI)
+                {
+
+                    return;
+                }
+                bool hasHit = GetRayHit(Position, out _hit);
+                if(hasHit)
+                {
+                    Debug.Log($"RaycastHit: {_hit.collider.gameObject.name}");
+                    OnAction_RaycastHit?.Invoke(_hit);
+                }
                 break;
             default:
                 break;
@@ -36,7 +56,7 @@ public class InputManager : MonoBehaviour
     public void MousePosition(InputAction.CallbackContext ctx)
     {
         Position = ctx.ReadValue<Vector2>();
-        Debug.Log($"Mouse Position: {Position}");
+        //Debug.Log($"Mouse Position: {Position}");
     }
     public void WheelClickAction(InputAction.CallbackContext ctx)
     {
@@ -62,5 +82,29 @@ public class InputManager : MonoBehaviour
             }
         }
         return false;
+    }
+    private bool IsUITouch(Vector2 point)
+    {
+        PointerEventData pointData = new PointerEventData(EventSystem.current)
+        {
+            position = point
+        };
+
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+
+        EventSystem.current.RaycastAll(pointData, raycastResults);
+        if(raycastResults.Count > 0)
+        {
+            foreach(var r in raycastResults)
+            {
+                if(r.gameObject.layer == LayerMask.NameToLayer("UI"))
+                {
+                    Debug.Log("IsUITouch: True");
+                    return true;
+                }
+            }
+        }
+        return false;
+
     }
 }
